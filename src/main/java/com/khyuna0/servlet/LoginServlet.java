fd47ab86c0037215336bd283211c39b84d22f20d
khyuna0 <khohyuna0102@gmail.com>
2025-08-07 12:11:25 +0900

    로그인 실패 시 메세지 출력 기능 추가

--- src/main/java/com/khyuna0/servlet/LoginServlet.java
+++ src/main/java/com/khyuna0/servlet/LoginServlet.java
@@ -41,8 +41,9 @@ public class LoginServlet extends HttpServlet {
 			response.sendRedirect("welcome.jsp"); // 로그인 성공 페이지로 이동
 		} else { //로그인 실패
 			//response.sendRedirect("loginFail.jsp"); // 로그인 실패 페이지로 이동
-			request.setAttribute("failId", mid); // 로그인 실패한 아이디
-			request.getRequestDispatcher("loginFail.jsp").forward(request, response);
+			//request.setAttribute("failId", mid); // 로그인 실패한 아이디
+			request.setAttribute("errorMsg", "아이디 또는 비밀번호가 잘못되었습니다. 다시 확인하세요.");
+			request.getRequestDispatcher("login.jsp").forward(request, response);
 		}
 		
 	}
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,5 +15,10 @@
 		비밀번호 : <input type="text" name="mpw"><br><br>
 		<input type="submit" value="로그인">
 	</form>
+	<hr>
+	<!-- 로그인 실패시 출력될 메세지 -->
+	<c:if test="${not empty errorMsg }">
+		<h4 style="color: red;">${errorMsg }</h4>
+	</c:if>	
 </body>
 </html>
